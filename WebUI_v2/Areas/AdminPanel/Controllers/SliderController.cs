0732888a547ba8d0a8369de489e98c9bbfbb61f5
farruxaliyev299 <7hp245d@code.edu.az>
2022-06-09 03:21:33 +0400

    Update 7

---
+++ WebUI_v2/Areas/AdminPanel/Controllers/SliderController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebUI_v2.DAL;
+
+namespace WebUI_v2.Areas.AdminPanel.Controllers
+{
+    [Area("AdminPanel")]
+    public class SliderController : Controller
+    {
+        private AppDbContext _context { get; }
+
+        public SliderController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Sliders);
+        }
+    }
+}
